8197700368f63657a38b7dd4766bdea448b8cab8
Eric Julianto <algonacci1@gmail.com>
2023-03-16 12:30:14 +0700

    finishing (WIP)

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import time
+import warnings
+
+import openai as ai
+import speech_recognition as sr
+import module as md
+
+
+warnings.filterwarnings("ignore")
+
+
+def listen_speak():
+    r = sr.Recognizer()
+    r.energy_threshold = 4000
+    r.dynamic_energy_threshold = True
+
+    with sr.Microphone(sample_rate=16000, chunk_size=512) as source:
+        print('Bang Eric mau nanya apa?')
+        say = 'Bang Eric mau nanya apa?'
+        md.jarvis(say)
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+
+        try:
+            message = r.recognize_google(audio, language='id', show_all=False)
+            print("You said: " + message)
+            response = md.generate(message)
+            print("ZiyonGPT: ", + response)
+            # md.jarvis(say)
+        except:
+            print("Please Repeat")
+            time.sleep(1)
+            # pass
+
+
+if __name__ == '__main__':
+    while True:
+        listen_speak()
---
+++ module.py
@@ -0,0 +1,32 @@
+import os
+
+import openai
+from dotenv import dotenv_values
+from gtts import gTTS
+from playsound import playsound
+
+config = dotenv_values(".env")
+
+openai.api_key = config["OPENAI_API_KEY"]
+
+audio_file = "jarvis_record.mp3"
+
+
+def jarvis(say):
+    myobj = gTTS(text=say, lang='id', slow=False)
+    myobj.save(audio_file)
+    playsound('jarvis_record.mp3')
+    os.remove(audio_file)
+
+
+def generate(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=50,
+        n=1,
+        stop=None,
+        timeout=15
+    )
+    return response.choices[0].text.strip()
